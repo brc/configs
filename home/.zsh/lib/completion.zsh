6bb74a3ff3709c490e5b1232f0f0716e26c8e2a2
Brett Campbell <invsblduck@gmail.com>
2014-03-09 13:22:24 -0700

    wrap long lines

--- home/.zsh/lib/completion.zsh
+++ home/.zsh/lib/completion.zsh
@@ -9,23 +9,42 @@ WORDCHARS=''
 
 zmodload -i zsh/complist
 
-zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*' matcher-list \
+    'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
 zstyle ':completion:*:*:*:*:*' menu select
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
-zstyle ':completion:*:*:*:*:processes'    command "ps -u `whoami` -o pid,user,comm -w -w"
-zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+zstyle ':completion:*:*:*:*:processes' \
+    command "ps -u `whoami` -o pid,user,comm -w -w"
+
+zstyle ':completion:*:*:kill:*:processes' \
+    list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
 
 # disable named-directories autocompletion
-zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
+zstyle ':completion:*:cd:*' \
+    tag-order local-directories directory-stack path-directories
+
 cdpath=(.)
 
-# use /etc/hosts and known_hosts for hostname completion
-[ -r /etc/ssh/ssh_known_hosts ] && _global_ssh_hosts=(${${${${(f)"$(</etc/ssh/ssh_known_hosts)"}:#[\|]*}%%\ *}%%,*}) || _global_ssh_hosts=()
-[ -r ~/.ssh/known_hosts ] && _ssh_hosts=(${${${${(f)"$(<$HOME/.ssh/known_hosts)"}:#[\|]*}%%\ *}%%,*}) || _ssh_hosts=()
-[ -r ~/.ssh/config ] && _ssh_config=($(cat ~/.ssh/config | sed -ne 's/Host[=\t ]//p')) || _ssh_config=()
-[ -r /etc/hosts ] && : ${(A)_etc_hosts:=${(s: :)${(ps:\t:)${${(f)~~"$(</etc/hosts)"}%%\#*}##[:blank:]#[^[:blank:]]#}}} || _etc_hosts=()
+# hostname completion
+_global_ssh_hosts=()
+_ssh_hosts=()
+_ssh_config=()
+_etc_hosts=()
+
+[ -r /etc/ssh/ssh_known_hosts ] && \
+    _global_ssh_hosts=(${${${${(f)"$(</etc/ssh/ssh_known_hosts)"}:#[\|]*}%%\ *}%%,*})
+
+[ -r ~/.ssh/known_hosts ] && \
+    _ssh_hosts=(${${${${(f)"$(<$HOME/.ssh/known_hosts)"}:#[\|]*}%%\ *}%%,*})
+
+[ -r ~/.ssh/config ] && \
+    _ssh_config=($(cat ~/.ssh/config | sed -ne 's/Host[=\t ]//p'))
+
+[ -r /etc/hosts ] && \
+    : ${(A)_etc_hosts:=${(s: :)${(ps:\t:)${${(f)~~"$(</etc/hosts)"}%%\#*}##[:blank:]#[^[:blank:]]#}}}
+
 hosts=(
   "$_ssh_config[@]"
   "$_global_ssh_hosts[@]"
