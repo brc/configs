a714ac86cea6be089ba39f5a83c4fc6575505ad9
Brett Campbell <brc@linux.com>
2025-08-31 01:58:53 -0700

    emacs: modularize config

---
+++ home/.emacs.d/core/packages.el
@@ -0,0 +1,44 @@
+;;; core/packages.el --- Package Management Configuration -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; Core package management setup extracted from Spacemacs.
+;; Handles package repositories, use-package installation, and configuration.
+
+;;; Code:
+
+;; Package Management Setup
+;; Based on Spacemacs core/core-configuration-layer.el
+
+;; Initialize package system
+(require 'package)
+
+;; Set package archives - using the same ones as Spacemacs
+(setq package-archives
+      '(("melpa"  . "https://melpa.org/packages/")
+        ("gnu"    . "https://elpa.gnu.org/packages/")
+        ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
+
+;; Set package directory
+(setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
+
+;; Initialize package system
+(package-initialize)
+
+;; Refresh package contents if we haven't done so
+;; (unless package-archive-contents
+;;   (package-refresh-contents))
+
+;; Install use-package if not already present
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+;; Load use-package
+(require 'use-package)
+
+;; Configure use-package defaults
+(setq use-package-always-ensure t    ; Always install packages if missing
+      use-package-verbose t          ; Show loading info
+      use-package-compute-statistics t) ; Enable statistics
+
+(provide 'core-packages)
+;;; core/packages.el ends here
--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -11,39 +11,12 @@
       (gc-cons-percentage 0.6)
       (file-name-handler-alist nil))
 
-  ;; Package Management Setup
-  ;; Based on Spacemacs core/core-configuration-layer.el
+  ;; Load core modules
+  (load (expand-file-name "core/packages" user-emacs-directory))
 
-  ;; Initialize package system
-  (require 'package)
-
-  ;; Set package archives - using the same ones as Spacemacs
-  (setq package-archives
-        '(("melpa"  . "https://melpa.org/packages/")
-          ("gnu"    . "https://elpa.gnu.org/packages/")
-          ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
-
-  ;; Set package directory
-  (setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
-
-  ;; Initialize package system
-  (package-initialize)
-
-  ;; Refresh package contents if we haven't done so
-  ;; (unless package-archive-contents
-  ;;   (package-refresh-contents))
-
-  ;; Install use-package if not already present
-  (unless (package-installed-p 'use-package)
-    (package-install 'use-package))
-
-  ;; Load use-package
-  (require 'use-package)
-
-  ;; Configure use-package defaults
-  (setq use-package-always-ensure t    ; Always install packages if missing
-        use-package-verbose t          ; Show loading info
-        use-package-compute-statistics t) ; Enable statistics
+  ;; Load feature modules (add more as we implement them)
+  ;; (load (expand-file-name "modules/evil" user-emacs-directory))
+  ;; (load (expand-file-name "modules/completion" user-emacs-directory))
 
   ;; Restore garbage collection settings after startup
   (add-hook 'emacs-startup-hook
