285baf98c31080baf7f8a0167c9c05405c31b850
Brett Campbell <brc@linux.com>
2025-09-01 00:53:14 -0700

    emacs: install doom-modeline

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -18,6 +18,7 @@
   ;; Load feature modules (add more as we implement them)
   (load (expand-file-name "modules/which-key" user-emacs-directory))
   (load (expand-file-name "modules/evil" user-emacs-directory))
+  (load (expand-file-name "modules/modeline" user-emacs-directory))
   ;; (load (expand-file-name "modules/completion" user-emacs-directory))
 
   ;; Restore garbage collection settings after startup
@@ -38,4 +39,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(evil-goggles-change-face ((t (:inherit diff-removed))))
+ '(evil-goggles-delete-face ((t (:inherit diff-removed))))
+ '(evil-goggles-paste-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
+ '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
+ '(evil-goggles-yank-face ((t (:inherit diff-changed)))))
---
+++ home/.emacs.d/modules/modeline.el
@@ -0,0 +1,32 @@
+;;; modeline.el --- Modeline configuration -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; Configure doom-modeline for a fancy status line
+
+;;; Code:
+
+;; Add doom-modeline to package list
+(add-to-list 'package-selected-packages 'doom-modeline)
+
+;; Ensure package is installed
+(unless (package-installed-p 'doom-modeline)
+  (package-install 'doom-modeline))
+
+;; Configure doom-modeline
+(use-package doom-modeline
+  :ensure t
+  :init
+  (doom-modeline-mode 1)
+  :config
+  ;; Show icons (requires all-the-icons package)
+  (setq doom-modeline-icon t)
+  ;; Show Evil state in modeline
+  (setq doom-modeline-modal t)
+  ;; Show buffer file name
+  (setq doom-modeline-buffer-file-name-style 'auto)
+  ;; Show line numbers
+  (setq doom-modeline-buffer-state-icon t)
+  ;; Height of the modeline
+  (setq doom-modeline-height 25))
+
+;;; modeline.el ends here
