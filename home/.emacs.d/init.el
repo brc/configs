06515f3ea0e70f5d7130b537dc16c2fcb3fdf7d6
Brett Campbell <brc@linux.com>
2025-09-08 00:31:33 -0700

    emacs: Use Helm

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -21,6 +21,7 @@
   (load (expand-file-name "modules/evil" user-emacs-directory))
   (load (expand-file-name "modules/default-leader-keys" user-emacs-directory))
   (load (expand-file-name "modules/modeline" user-emacs-directory))
+  (load (expand-file-name "modules/helm" user-emacs-directory))
   ;; (load (expand-file-name "modules/completion" user-emacs-directory))
 
   ;; Restore garbage collection settings after startup
@@ -36,6 +37,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages nil))
+ '(helm-minibuffer-history-key "M-p")
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ home/.emacs.d/modules/helm.el
@@ -0,0 +1,77 @@
+;;; modules/helm.el --- Helm Configuration -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; Essential Helm packages and configuration extracted from Spacemacs helm layer.
+;; This provides the core Helm functionality without the full complexity of 47+ packages.
+
+;;; Code:
+
+;; =============================================================================
+;; CORE HELM PACKAGES
+;; =============================================================================
+
+;; Install helm-core first (required dependency)
+(use-package helm-core
+  :ensure t)
+
+;; Main helm package
+(use-package helm
+  :ensure t
+  :after helm-core
+  :init
+  ;; Use helm by default for M-x, C-x C-f, and C-x b
+  (global-set-key (kbd "M-x") 'helm-M-x)
+  (global-set-key (kbd "C-x C-f") 'helm-find-files)
+  (global-set-key (kbd "C-x b") 'helm-buffers-list)
+
+  :config
+  ;; Enable helm mode for better integration
+  (helm-mode 1)
+
+  ;; Basic helm configuration
+  (setq helm-split-window-inside-p t  ; open helm buffer inside current window
+        helm-move-to-line-cycle-in-source t  ; move to end or beginning of source when reaching top or bottom
+        helm-ff-search-library-in-sexp t  ; search for library in require and declare-function sexp
+        helm-scroll-amount 8  ; scroll 8 lines other window using M-<next>/M-<prior>
+        helm-ff-file-name-history-use-recentf t
+        helm-M-x-fuzzy-match t  ; Enable fuzzy matching for M-x
+        helm-buffers-fuzzy-matching t  ; Enable fuzzy matching for buffers
+        helm-recentf-fuzzy-match t))  ; Enable fuzzy matching for recent files
+
+;; =============================================================================
+;; ESSENTIAL HELM EXTENSIONS (will be added in later modules)
+;; =============================================================================
+
+;; Note: helm-swoop, helm-projectile, etc. will be added in separate modules
+;; to keep this core module focused on basic helm functionality
+
+;; =============================================================================
+;; SPACEMACS-STYLE FUNCTIONS
+;; =============================================================================
+
+(defun spacemacs/helm-M-x-fuzzy-matching ()
+  "Helm M-x with fuzzy matching enabled"
+  (interactive)
+  (let ((completion-styles completion-styles))
+    (add-to-list 'completion-styles 'flex t)
+    (call-interactively 'helm-M-x)))
+
+(defun spacemacs/helm-find-files ()
+  "Custom helm-find-files with Spacemacs behavior."
+  (interactive)
+  (helm-find-files nil))
+
+;; =============================================================================
+;; LEADER KEY BINDINGS
+;; =============================================================================
+
+;; Replace existing placeholder bindings with helm versions
+(spacemacs/set-leader-keys
+  "SPC" 'spacemacs/helm-M-x-fuzzy-matching  ; SPC SPC for M-x
+  "bb"  'helm-mini                          ; Buffer list (better than switch-to-buffer)
+  "ff"  'spacemacs/helm-find-files          ; Find files
+  "fr"  'helm-recentf                       ; Recent files (better than placeholder)
+  "ry"  'helm-show-kill-ring                ; Kill ring
+  "rl"  'helm-resume)                       ; Resume last helm session
+
+;;; modules/helm.el ends here
